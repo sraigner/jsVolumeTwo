3498b54142518aef53706fa04b138b3d452ea1b7
Scott Raigner <scottraigner@comcast.net>
2018-05-10 11:18:35 -0700

    Adding more files.

---
+++ scripts/Array2.js
@@ -0,0 +1,14 @@
+// Array two
+
+var friends =["Mark","Lisa","Denny","Peter","Steven","Claudette"];
+
+function greetFriends() {
+  
+ for(var i =0 ; i < friends.length; i++) {
+   console.log("Hello " + friends[i]);
+   
+ }
+  
+}
+
+greetFriends();
---
+++ scripts/Functions2.js
@@ -0,0 +1,20 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+var name;
+
+function sayHello(name) {
+            console.log("Oh hi " + name);
+            }
+            
+sayHello("Mark");
+sayHello("Benny");
+sayHello("Linda");
+sayHello();
+
